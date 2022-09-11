3b47c2e86fabc195bb0d36e7b38393166b864eee
coco <iljcoco@163.com>
2022-09-11 18:55:27 +0800

    活动申请-事务管理器

--- nanqiao-api/src/test/java/com/example/nanqiao/api/controllers/ActivityApplyControllerTest.java
+++ nanqiao-api/src/test/java/com/example/nanqiao/api/controllers/ActivityApplyControllerTest.java
@@ -1,5 +1,6 @@
-package com.example.nanqiao.api;
+package com.example.nanqiao.api.controllers;
 
+import com.example.nanqiao.api.NanqiaoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
