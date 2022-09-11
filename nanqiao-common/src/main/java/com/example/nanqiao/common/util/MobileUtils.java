504b6697e6345485ed190bd35a803f6e304ec6e6
yintianye <yintianye@htffund.com>
2022-09-11 20:15:32 +0800

    refactor on MobileUtils.

--- nanqiao-common/src/main/java/com/example/nanqiao/common/util/MobileUtils.java
+++ nanqiao-common/src/main/java/com/example/nanqiao/common/util/MobileUtils.java
@@ -13,19 +13,16 @@ import java.util.regex.Pattern;
  **/
 @Component
 public class MobileUtils {
-    private static final String regex = "^1[3|4|5|6|7|8|9][0-9]\\d{4,8}$";
+    private static final String regex = "^1[3-9][0-9]{9}$";
 
     public static Boolean checkMobile(String mobile) {
-        if (mobile.length() != 11) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(mobile);
+        boolean isMatch = m.matches();
+        if (!isMatch) {
             throw new BaseException(NanQiaoErrorCode.PARAM_ILLEGAL,"请输入正确的手机号");
-        } else {
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(mobile);
-            boolean isMatch = m.matches();
-            if (!isMatch) {
-                throw new BaseException(NanQiaoErrorCode.PARAM_ILLEGAL,"请输入正确的手机号");
-            }
         }
+
         return true;
     }
 }
