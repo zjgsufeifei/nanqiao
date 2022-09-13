cf9eb1a1c2cf6db0417f9243c039d2c6f563fd94
ruge <ruge@pinduoduo.com>
2022-09-13 21:21:25 +0800

    modify

--- nanqiao-common/src/main/java/com/example/nanqiao/common/request/file/FileUploadRequest.java
+++ nanqiao-common/src/main/java/com/example/nanqiao/common/request/file/FileUploadRequest.java
@@ -4,10 +4,13 @@ import com.example.nanqiao.common.enums.ContentCatalogTypeEnum;
 import com.example.nanqiao.common.request.BaseRequest;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author smile
  */
 @Data
 public class FileUploadRequest extends BaseRequest {
+    @NotNull(message = "类目类型不能为空")
     private ContentCatalogTypeEnum type;
 }
