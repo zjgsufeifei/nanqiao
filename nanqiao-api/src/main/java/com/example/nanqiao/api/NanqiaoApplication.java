4705f590141fd9511cadb6ba3c236098eb69e68b
ruge <ruge@pinduoduo.com>
2022-09-13 19:15:00 +0800

    add swagger

--- nanqiao-api/src/main/java/com/example/nanqiao/api/NanqiaoApplication.java
+++ nanqiao-api/src/main/java/com/example/nanqiao/api/NanqiaoApplication.java
@@ -6,14 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @Import({CoreConfiguration.class})
 public class NanqiaoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NanqiaoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NanqiaoApplication.class, args);
+    }
 }
